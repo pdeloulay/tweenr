70ebd5763e9b8a0dd8c6b6841e19daa7686dcaa1
Patrick Deloulay <pdeloulay@gmail.com>
2014-04-11 18:12:53 -0700

    Basic ladder in place
    Bootstrap added

--- js/app.js
+++ js/app.js
@@ -1,47 +1,59 @@
-App = Ember.Application.create();
+App = Ember.Application.create({
+  LOG_TRANSITIONS: true
+});
 
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 App.Router.map(function() {
-  // put your routes here
-  this.route("players", { path: "/players" });
+  this.resource("players", function() {
+    this.resource("player", { path: "/:player_id" });  
+  });
+  this.resource("clubs");
 });
 
 App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
+  redirect: function() {
+    this.transitionTo("players");
   }
 });
 
 App.PlayersRoute = Ember.Route.extend({
   model: function() {
-    return App.Player.FIXTURES;
+    return this.store.findAll("Player");
   }
 });
 
+App.PlayerRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find("Player", params.player_id);
+  }
+})
 
-App.ApplicationAdapter = DS.FixtureAdapter;
+App.PlayersController = Ember.ArrayController.extend({
+  sortProperties: ["lastName"]
+});
 
+
+App.Player = DS.Model.extend({
+  firstName: DS.attr( 'string' ),
+  lastName: DS.attr( 'string' ),
+  usta: DS.attr('string'),
+});
+
+App.Player.FIXTURES = [
+  { id: 1, firstName: 'Patrick', lastName: 'Deloulay', usta: '4.5C' },
+  { id: 2, firstName: 'Tom' , lastName: 'Dale', usta: '4.0' },
+  { id: 3, firstName: 'Kyle', lastName: 'Warren', usta: '4.5C' },
+  { id: 4, firstName: 'John' , lastName: 'Smith', usta: '5.0' }
+];
 
 App.Club = DS.Model.extend({
   name: DS.attr( 'string' ),
   geo: DS.attr( 'string' )
 });
 
-App.Player = DS.Model.extend({
-  firstName: DS.attr( 'string' ),
-  lastName: DS.attr( 'string' ),
-  usta: DS.attr('string')
-});
-
-App.Player.FIXTURES = [
-  { id: 1, firstName: 'Patrick', lastName: 'Deloulay', usta: '4.5C' },
-  { id: 2, firstName: 'Tom' , lastName: 'Dale', usta: '4.0'     },
-  { id: 3, firstName: 'Kyle', lastName: 'Warren', usta: '4.5C' },
-  { id: 4, firstName: 'John' , lastName: 'Smith', usta: '5.0'     }
-];
-
 App.Club.FIXTURES = [
-  { id: 1, name: 'Bay Club Santa Clara', geo: '37.376712,-121.985984'},
-  { id: 1, name: 'Bay Club Cupertino', geo: '37.32435,-122.01569'},
-  { id: 1, name: 'Bay Club Marin', geo: '30.24805,-85.62918'}
+  { id: 1, name: 'Bay Club Santa Clara', geo: '37.376712,-121.985984' },
+  { id: 2, name: 'Bay Club Cupertino', geo: '37.32435,-122.01569' },
+  { id: 3, name: 'Bay Club Marin', geo: '30.24805,-85.62918' }
 ];
